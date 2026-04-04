b8fe97b402e1d5be495e2207588e27d4fc8f854c
Cinziabast <josegiovanni2022@hotmail.com>
2026-04-04 13:13:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// HERO SLIDER
+let slides = document.querySelectorAll('.hero-slide');
+let currentSlide = 0;
 
+function changeSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+setInterval(changeSlide, 4000);
